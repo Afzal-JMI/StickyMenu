b802372a8f618ad120ad52cdcadc2e6fb005739f
Afzal-JMI <164883558+Afzal-JMI@users.noreply.github.com>
2024-05-29 10:14:42 +0530

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,6 +1,6 @@
 console.log(document.querySelectorAll('img')[1]);
 
-document.querySelectorAll('img')[1].style.height = '200px';
+document.querySelectorAll('img')[1].style.height = '100px';
 document.querySelectorAll('img')[0].style.height = '100px';
 document.querySelectorAll('img')[1].style.width = '100%'
 console.log(window.pageYOffset);
